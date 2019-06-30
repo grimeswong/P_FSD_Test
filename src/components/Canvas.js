7ae781b85d82bf7fbf1d67ab43cb4a41aaefd3a4
Shadow Grimes <grimeswongatwork@gmail.com>
2019-06-30 14:43:57 +1000

    Define Canvas basic functions outline

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -9,18 +9,53 @@ class Canvas extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      colour: "#000000"
+      mousePress: true,
+      colour: "#000000",
+      lastX: 0,
+      lastY: 0,
+      totalClick: 0,
+      sessionClick: 0
     }
+    this.mousedown = this.mousedown.bind(this)
+    this.mousemove = this.mousemove.bind(this)
+    this.mouseup = this.mouseup.bind(this)
+    this.mouseleave = this.mouseleave.bind(this)
+    this.draw = this.draw.bind(this)
+  }
+
+  mousedown(e) {
+    console.log("mousedown");
+  }
+
+  mousemove(e) {
+    console.log("mousemove");
+  }
+
+  mouseup(e) {
+    console.log("mouseup");
+  }
+
+  mouseleave(e) {
+    console.log("mouseleave");
+  }
+
+  draw() {
+
   }
 
   render() {
     return(
-      <div className="container-canvas">
-
+      <div className="container-canvas"
+                      onMouseDown={this.mousedown}
+                      onMouseMove={this.mousemove}
+                      onMouseUp={this.mouseup}
+                      onMouseLeave={this.mouseleave}
+                      >
       </div>
     )
 
   }
+
 }
 
 export default Canvas;
