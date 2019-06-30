5f4f6a45973b1ca31d2ea3b2c15dd56460810b68
Shadow Grimes <grimeswongatwork@gmail.com>
2019-06-30 13:09:28 +1000

    Create a Canvas component and basic element and scss sheet setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import TopBar from './components/TopBar';
+import Canvas from './components/Canvas';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Canvas />
     </div>
   );
 }
---
+++ src/components/Canvas.js
@@ -0,0 +1,21 @@
+/*
+    This component is for user to draw on the screen
+*/
+
+import React, {Component} from 'react';
+
+class Canvas extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h1>testing Canvas</h1>
+    )
+
+  }
+}
+
+export default Canvas;
